038211212ca078f1e20edb962dc0f6919a70e346
rodollp <zeus02021101@gmail.com>
2026-06-12 15:56:37 +0900

    update

---
+++ 6-12Unity/Assets/Script/MiroGrid.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class MiroGrid : MonoBehaviour
+{
+    [Header("»óÇÏÁÂ¿ì")]
+    [SerializeField] private int leftRight = 3;
+    [SerializeField] private int UpDown =  3;
+    
+
+
+}
