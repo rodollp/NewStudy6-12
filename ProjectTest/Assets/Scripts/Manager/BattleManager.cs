6e7283407d0db835772a331fdfcebdd6e6703f96
rodollp <zeus02021101@gmail.com>
2026-06-18 17:22:54 +0900

    repac

--- ProjectTest/Assets/Scripts/Manager/BattleManager.cs
+++ ProjectTest/Assets/Scripts/Manager/BattleManager.cs
@@ -3,31 +3,24 @@ using UnityEngine;
 public class BattleManager : MonoBehaviour
 {
     [SerializeField] PlayerStatus player;
+    [SerializeField] StageManager stageManager;
+
+    public void RegisterMonster(Monster monster)
+    {
+        stageManager.AddMonster(monster);
+
+        monster.OnDead += MonsterDeadEvent;
+    }
 
     public void MonsterDeadEvent(Monster monster)
     {
         TakeReward(monster);
-        DropItem(monster);
+        stageManager.OnMonsterDead(monster);
     }
 
     void TakeReward(Monster monster)
     {
         player.AddExp(monster.Reward.ExpReward);
         player.AddGold(monster.Reward.Gold);
-    }
-
-    void DropItem(Monster monster)
-    {
-        if (monster.dropItem == null) return;
-        if (monster.dropItem.Length == 0) return;
-
-        int index = Random.Range(0, monster.dropItem.Length);
-
-        GameObject itemPrefab = monster.dropItem[index];
-
-        if (itemPrefab == null) return;
-        Vector3 spawnPoint = monster.transform.position + Vector3.up * 1f;
-        Instantiate(itemPrefab, spawnPoint, Quaternion.identity);
-        Debug.Log("아이템 드랍!");
     }
 }
